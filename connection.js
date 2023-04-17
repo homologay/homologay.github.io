69de132c821d327305e786fd455442532c02c8ec
maxine <maxbeckie@tutanota.com>
2023-04-16 18:00:41 -0700

    fix again

--- connection.js
+++ connection.js
@@ -1,21 +1,21 @@
-let permission = Notification.permission;
-
+// let permission = Notification.permission;
+/*
 if (permission === "granted") {   
        showNotification();
 } else if (permission === "default") {   
-       requestAndShowPermission();
+      
 } else {  
        alert("Use normal alert");
-}
+}*/
 
 function showNotification() {   
-       if (document.visibilityState === "visible") {
-              return;
-       }   
-       var title = "JavaScript Jeep";   
-       icon = "image-url"   
-       var body = "Message to be displayed";   
-       var notification = new Notification('Title', { body, icon });   
+      // if (document.visibilityState === "visible") {
+      //        return;
+      // }   
+      //  var title = "joke";   
+       icon = "blinkies/connection_established.gif"   
+       var body = "not malware";   
+       var notification = new Notification('joke', { body, icon });   
        notification.onclick = () => { 
           notification.close();
           window.parent.focus();
