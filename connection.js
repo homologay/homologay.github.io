108c0062362547c3e9a1110ba5fde09ccdbe9e6d
maxine <maxbeckie@tutanota.com>
2023-04-16 18:37:48 -0700

    a

--- connection.js
+++ connection.js
@@ -1,14 +1,3 @@
-// let permission = Notification.permission;
-/*
-if (permission === "granted") {   
-       showNotification();
-} else if (permission === "default") {   
-      
-} else {  
-       alert("Use normal alert");
-}*/
-
-
 function showNotification() {   
        icon = "blinkies/connection_established.gif"   
        var body = "not malware";   
@@ -18,15 +7,6 @@ function showNotification() {
           window.parent.focus();
        }
 }
-/*
-function requestAndShowPermission() {
-   Notification.requestPermission(function (permission) {
-      if (permission === "granted") {
-            showNotification();
-      }
-   });
-}
-*/
 
 // on some browsers like safari or firefox notifications may be 
 // requested through a click event but not on page load
