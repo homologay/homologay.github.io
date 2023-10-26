6f41fe98f0cf3b61e39e7d2d65963e7cdbe263fb
Maxine <maxbeckie@tutanota.com>
2023-10-26 16:20:02 -0700

    comic sans and no wasm
    -redundant since fractal gen is wasm

--- wasm-fun/julia_bg.wasm.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function greet(): void;
-export function get_ip_addr_of_request(): void;
-export function ping_ip_addr(): void;
