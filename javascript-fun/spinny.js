df513a5eeb9ad8136f5bcf45101206083b215eae
Maxine <maxbeckie@tutanota.com>
2023-10-26 16:08:02 -0700

    spinny but doesn't work

---
+++ javascript-fun/spinny.js
@@ -0,0 +1,21 @@
+R = 0;
+x1 = .1;
+y1 = .05;
+x2 = .25;
+y2 = .24;
+x3 = 1.6;
+y3 = .24;
+x4 = 300;
+y4 = 200;
+x5 = 300;
+y5 = 200;
+DI = document.getElementsByTagName("h1");
+DIL = DI.length;
+  
+
+function spin() {
+  for (i = 0; i - DIL; i++) {
+    // idk do something .....
+    console.log("uhhh to be implemented");
+  } 
+}
