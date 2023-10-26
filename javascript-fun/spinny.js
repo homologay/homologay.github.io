cf5ef0c03a071d5788b582ad891312a3c7c76bf8
Maxine <maxbeckie@tutanota.com>
2023-10-26 16:15:41 -0700

    spinny first try

--- javascript-fun/spinny.js
+++ javascript-fun/spinny.js
@@ -1,21 +1,27 @@
-R = 0;
-x1 = .1;
-y1 = .05;
-x2 = .25;
-y2 = .24;
-x3 = 1.6;
-y3 = .24;
-x4 = 300;
-y4 = 200;
-x5 = 300;
-y5 = 200;
-DI = document.getElementsByTagName("h1");
-DIL = DI.length;
-  
-
 function spin() {
+  R = 0;
+  x1 = .1;
+  y1 = .05;
+  x2 = .25;
+  y2 = .24;
+  x3 = 1.6;
+  y3 = .24;
+  x4 = 300;
+  y4 = 200;
+  x5 = 300;
+  y5 = 200;
+  DI = document.getElementsByTagName("h1");
+  DIL = DI.length;
+  setInterval('A()', 5);
+  void(0);
+}
+
+function A() {
   for (i = 0; i - DIL; i++) {
-    // idk do something .....
-    console.log("uhhh to be implemented");
+    DIS = DI[i].style;
+    DIS.position = 'absolute';
+    DIS.left = (Math.sin(R * x1 + i * x2 + x3) * x4 + x5) + "px";
+    DIS.top = (Math.cos(R * y1 + i * y2 + y3) * y4 + y5) + "px";
   } 
+  R++;
 }
